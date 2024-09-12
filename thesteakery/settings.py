c5f29546f0e15e1e7c044f772b1d4e90d3db0a82
Michael Drew <michaeldrew_@outlook.com>
2024-09-12 20:10:35 +0000

    updated settings

--- thesteakery/settings.py
+++ thesteakery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s^8m8nnfkx84dr5-9lld#hgu#8sz427xe^x&c&(1^kpvpanspn'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
