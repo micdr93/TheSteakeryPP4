48886dbca4989fa0f06bd05aba1bf23c7937181c
Michael Drew <michaeldrew_@outlook.com>
2024-09-14 17:43:42 +0000

    update models

--- restaurant/models.py
+++ restaurant/models.py
@@ -14,19 +14,7 @@ class SpecialRequest(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class MenuItem(models.Model):
-    """
-    Add a new menu item as Admin in admin panel.
-    """
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = CloudinaryField('menu_item_images', blank=True, null=True)
-
-    def __str__(self):
-        return self.name
+        
 class Booking(models.Model):
     """
     Restaurant Booking
