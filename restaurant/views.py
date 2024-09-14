e908d1e51c315e4c42cd9cc7fd63b0c9344c9c81
Michael Drew <michaeldrew_@outlook.com>
2024-09-14 18:18:04 +0000

    update views

--- restaurant/views.py
+++ restaurant/views.py
@@ -27,3 +27,10 @@ def your_reservation_view(request):
         return JsonResponse({'message': 'Booking successfully made!'})
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+    from django.shortcuts import render
+from .models import MenuItem
+
+def menu_view(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'menu.html', {'menu_items': menu_items})
