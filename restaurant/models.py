63cabdd6a8ac01e12590d5af34e99d5b8c811911
Michael Drew <michaeldrew_@outlook.com>
2024-09-14 14:36:04 +0000

    update models

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,6 +10,19 @@ class Booking(models.Model):
 
     def __str__(self):
         return f'{self.resource.name} booked by {self.user.username}'
+        
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+    special_requests = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} at {self.time}"
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
