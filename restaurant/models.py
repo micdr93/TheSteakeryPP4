83ee0149d991b895b922f9e5f1a1c9ddc776cb04
Michael Drew <michaeldrew_@outlook.com>
2024-09-14 14:29:51 +0000

    update models

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,30 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-slug = models.SlugField(max_length=200, unique=True)
-author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
-status = models.IntegerField(choices=STATUS, default=0)
-STATUS = ((0, "Draft"), (1, "Published"))
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(default='No description available') 
-    def __str__(self):
-        return self.name
-
-class Resource(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(default='No description available')
-
-    def __str__(self):
-        return self.name
-
 class Booking(models.Model):
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
