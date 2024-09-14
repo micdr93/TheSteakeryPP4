676bd4c481bfe7be98c8ca5bb3f99d98076dd8be
Michael Drew <michaeldrew_@outlook.com>
2024-09-14 21:11:24 +0000

    updated forms to include table selection

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,11 +1,21 @@
 from django import forms
-from .models import Booking
+from .models import Booking, Table
 
 class BookingForm(forms.ModelForm):
+    table = forms.ModelChoiceField(
+        queryset=Table.objects.all(),
+        label="Table Selection",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        help_text="Select an available table"
+    )
+    
     class Meta:
         model = Booking
-        fields = ['date', 'time', 'num_guests', 'special_requests']
+        fields = ['date', 'time', 'num_guests', 'special_requests', 'table']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
+            'num_guests': forms.NumberInput(attrs={'min': 1}),
+            'special_requests': forms.Textarea(attrs={'rows': 3}),
         }
+        
