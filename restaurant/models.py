2f14bf2a70f89cdd66c3d04d7a9c24c69e2b5c11
Michael Drew <michaeldrew_@outlook.com>
2024-09-14 15:44:31 +0000

    updated models to remove bug

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,15 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class Booking(models.Model):
-    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
-    def __str__(self):
-        return f'{self.resource.name} booked by {self.user.username}'
-        
+      
 class Booking(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
