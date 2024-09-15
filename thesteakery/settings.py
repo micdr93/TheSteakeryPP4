b8761b752e6dd531ab0402673d14598e6903b628
Michael Drew <michaeldrew_@outlook.com>
2024-09-15 21:17:08 +0000

    updated settings for heroku

--- thesteakery/settings.py
+++ thesteakery/settings.py
@@ -29,9 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get("HEROKU_HOST_NAME"), '*']
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOST_NAME"), '*', '.herokuapp.com']
 
 
 # Application definition
