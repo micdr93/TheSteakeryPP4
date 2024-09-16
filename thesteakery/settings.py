ba168ddbb1753291a994d4a039de0e88fa8e1686
Michael Drew <michaeldrew_@outlook.com>
2024-09-16 09:13:12 +0000

    update settings

--- thesteakery/settings.py
+++ thesteakery/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = 'thesteakerypp4-443f2b4046b5.herokuapp.com', '*', '.herokuapp.com', '8000-micdr93-thesteakerypp4-gdtgqxzlvde.ws-eu116.gitpod.io'
 
